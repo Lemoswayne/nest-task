1f031260ecb6d7420b9c3296fb64fb2276e1089f
Lemoswayne <jpxavierlemos@hotmail.com>
2025-06-03 19:38:34 -0300

    Add environment variable validation with Joi and update database configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,32 @@ import { UserModule } from 'src/user/user.module';
 import { BoardModule } from 'src/board/board.module';
 import { TaskModule } from 'src/task/task.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from 'src/task/entities/task.entity';
-import { User } from 'src/user/entities/user.entity';
-import { Board } from 'src/board/entities/board.entity';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // Evitar que esqueça de definir as variáveis de ambiente em produção
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.string().valid('postgres').required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_DATABASE: Joi.string().required(),
+        DATABASE_AUTOLOADENTITIES: Joi.number().default(1).valid(0, 1),
+        DATABASE_SYNCHRONIZE: Joi.number().default(1).valid(0, 1),
+      }),
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '123456',
-      entities: [Task, User, Board],
-      autoLoadEntities: true, // Carrega automaticamente as entidades
-      synchronize: true, // Sincroniza o banco de dados com as entidades (não recomendado para produção)
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT || 5432),
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Desligar em produção
     }),
     UserModule,
     BoardModule,
